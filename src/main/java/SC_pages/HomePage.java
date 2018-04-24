6b2449968fb537fbb68f46fd3376512ea8d0efe4
Hieu Tran <hieutran@Smartdevs-Mac-mini.local>
2018-04-24 19:08:55 +0700

    add

--- src/main/java/SC_pages/HomePage.java
+++ src/main/java/SC_pages/HomePage.java
@@ -1,5 +1,6 @@
 package SC_pages;
 
+import SC_interfaces.AbstractPageUI;
 import SC_interfaces.HomePageUI;
 import commons.AbstractMobile;
 import io.appium.java_client.AppiumDriver;
@@ -16,6 +17,12 @@ public class HomePage extends AbstractMobile {
 		clickToElement(driverAppium, HomePageUI.MY_ACCOUNT_MENU);
 		clickToElement(driverAppium, HomePageUI.SIGN_UP_MENU);
 		return PageFactory.getSignUpPage(driverAppium);
+	}
+
+	// open each page
+	public HomePage openHomePage(AppiumDriver<MobileElement> driverAppium) {
+		clickToElement(driverAppium, AbstractPageUI.HOME_PAGE);
+		return PageFactory.getHomePage(driverAppium);
 	}
 
 }
