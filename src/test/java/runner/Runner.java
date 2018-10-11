bf9f937e7d8a3ba6edd4ce04f2a3ee1b4a728213
hieutrandn9889 <hieutrandn123@gmail.com>
2018-10-11 22:36:44 +0700

    add

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -9,10 +9,10 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 @CucumberOptions(
 		format	 = {"pretty", "json:target//json//cucumber.json", "html:target//html//"},
 //		features = {"src//test//resources//features//ZP//RegisterTo_ZP_Scenario.feature"}, 
-		features = {"src//test//resources//features//BT//skipScreen_BT_Scenario.feature"}, 
+		features = {"src//test//resources//features"}, 
 //		features = { "src/test/java/features/Register.feature" }, 
-		glue = { "stepdefinations","utility" }
-//		tags = {"@Medication"}
+		glue = { "stepdefinations","utility" },
+		tags = {"@Chrome"}
 )
 
 @Test
